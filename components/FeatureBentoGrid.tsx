c1db5b82910b31b3f1d234f0f586c8b4bc1146c4
Oranut Suphantamat <Oranut_s@kkumail.com>
2026-01-22 18:17:49 +0700

    add authetication

--- components/FeatureBentoGrid.tsx
+++ components/FeatureBentoGrid.tsx
@@ -117,7 +117,7 @@ const SkeletonTwo = () => {
           key={"skelenton-two" + i}
           variants={variants}
           style={{
-            maxWidth: Math.random() * (100 - 40) + 40 + "%",
+         maxWidth: i % 2 === 0 ? "90%" : "60%",
           }}
           className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4"
         ></motion.div>
